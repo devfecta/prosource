f0922653926d6f23aa2643e4274dde744cf3abbd
Kevin Kelm <kkelm@madisoncollege.edu>
2026-04-04 23:08:24 -0500

    Updated the drawer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
 import Container from '@mui/material/Container';
 import { Box, CssBaseline } from '@mui/material';
 
@@ -13,31 +15,45 @@ import './App.css';
 
 
 function App() {
+
+	const theme = createTheme({
+		palette: {
+			primary: {
+				main: "#88c343",
+				contrastText: '#000000',
+			},
+			secondary: {
+				main: "#8542C2",
+				contrastText: '#ffffff',
+			},
+		},
+	});
+
   return (
-    
-    
-    <Router basename="/">
-	
-		<CssBaseline />
+    <ThemeProvider theme={theme}>
+		<Router basename="/">
 		
-		<Header />
+			<CssBaseline />
+			
+			<Header />
 
+			
+
+			<Box component="main" flexGrow={1}>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					{/* <Route path="/services" element={<Services title={"Services - " + document.title} />} />
 		
+					<Route path="/confirmSubscription" Component={ConfirmSubscription} />
 
-		<Box component="main" flexGrow={1}>
-			<Routes>
-				<Route path="/" element={<Home />} />
-				{/* <Route path="/services" element={<Services title={"Services - " + document.title} />} />
-	
-				<Route path="/confirmSubscription" Component={ConfirmSubscription} />
+					<Route path="/portfolio/prosource" element={<Portfolio.Prosource technologies={this.state.workExperience.technologies} />} />	 */}
+				</Routes>
+			</Box>
 
-				<Route path="/portfolio/prosource" element={<Portfolio.Prosource technologies={this.state.workExperience.technologies} />} />	 */}
-			</Routes>
-		</Box>
+			<Footer />
 
-		<Footer />
-
-	</Router>
+		</Router>
+	</ThemeProvider>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,18 @@
 import { Component, useState } from 'react';
-import { Box, Container, AppBar, Drawer, Toolbar, Button, IconButton, Typography, Icon } from '@mui/material';
+import { styled, useTheme } from '@mui/material/styles';
+import { 
+    Box, Container, Toolbar, Button, IconButton, Typography, Icon, 
+    List, ListItem, ListItemButton, ListItemIcon, ListItemText, Divider
+} from '@mui/material';
+import MuiAppBar from '@mui/material/AppBar';
+import MuiDrawer from '@mui/material/Drawer';
 import MenuIcon from '@mui/icons-material/Menu';
-// import Button from '@mui/material/Button';
-// import Typography from '@mui/material/Typography';
-// import Container from '@mui/material/Container';
-// import { FaWordpress } from 'react-icons/fa';
+import HomeIcon from '@mui/icons-material/Home';
+import AtmIcon from '@mui/icons-material/Atm';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+
+
 // import Portfolio from './portfolio/Portfolio';
 
 
@@ -12,16 +20,153 @@ import MenuIcon from '@mui/icons-material/Menu';
 export default function Header (props) {
 
     //render() {
+        const theme = useTheme();
 
         const [open, setOpen] = useState(false);
 
         const toggleDrawer = (newOpen) => () => {
             setOpen(newOpen);
         };
+
+        const handleDrawerOpen = () => {
+            setOpen(true);
+        };
+
+        const handleDrawerClose = () => {
+            setOpen(false);
+        };
+
+        const drawerWidth = 240;
+
+        const openedMixin = (theme) => ({
+        width: drawerWidth,
+        transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+        overflowX: 'hidden',
+        });
+
+        const closedMixin = (theme) => ({
+        transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        overflowX: 'hidden',
+        width: `calc(${theme.spacing(7)} + 1px)`,
+        [theme.breakpoints.up('sm')]: {
+            width: `calc(${theme.spacing(8)} + 1px)`,
+        },
+        });
+
+        const DrawerHeader = styled('div')(({ theme }) => ({
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        padding: theme.spacing(0, 1),
+        // necessary for content to be below app bar
+        ...theme.mixins.toolbar,
+        }));
+
+        const AppBar = styled(MuiAppBar, {
+        shouldForwardProp: (prop) => prop !== 'open',
+        })(({ theme }) => ({
+        zIndex: theme.zIndex.drawer + 1,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        variants: [
+            {
+            props: ({ open }) => open,
+            style: {
+                marginLeft: drawerWidth,
+                width: `calc(100% - ${drawerWidth}px)`,
+                transition: theme.transitions.create(['width', 'margin'], {
+                easing: theme.transitions.easing.sharp,
+                duration: theme.transitions.duration.enteringScreen,
+                }),
+            },
+            },
+        ],
+        }));
+
+        const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+        ({ theme }) => ({
+            width: drawerWidth,
+            flexShrink: 0,
+            whiteSpace: 'nowrap',
+            boxSizing: 'border-box',
+            variants: [
+            {
+                props: ({ open }) => open,
+                style: {
+                ...openedMixin(theme),
+                '& .MuiDrawer-paper': openedMixin(theme),
+                },
+            },
+            {
+                props: ({ open }) => !open,
+                style: {
+                ...closedMixin(theme),
+                '& .MuiDrawer-paper': closedMixin(theme),
+                },
+            },
+            ],
+        }),
+        );
+
+        const DrawerMenu = (
+            <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+                <List>
+                    <ListItem disablePadding>
+                        <ListItemButton component="a" href="/">
+                            <ListItemIcon>
+                                <HomeIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="Home" sx={[ open ? { opacity: 1, } : { opacity: 0, }, ]} />
+                        </ListItemButton>
+                    </ListItem>
+                    <ListItem disablePadding>
+                        <ListItemButton component="a" href="/atms">
+                            <ListItemIcon>
+                                <AtmIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="ATMs" sx={[ open ? { opacity: 1, } : { opacity: 0, }, ]} />
+                        </ListItemButton>
+                    </ListItem>
+                </List>
+                {/* <List>
+                    {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                    <ListItem key={text} disablePadding>
+                        <ListItemButton>
+                        <ListItemIcon>
+                            {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                        </ListItemIcon>
+                        <ListItemText primary={text} />
+                        </ListItemButton>
+                    </ListItem>
+                    ))}
+                </List>
+                <Divider />
+                <List>
+                    {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                    <ListItem key={text} disablePadding>
+                        <ListItemButton>
+                        <ListItemIcon>
+                            {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                        </ListItemIcon>
+                        <ListItemText primary={text} />
+                        </ListItemButton>
+                    </ListItem>
+                    ))}
+                </List> */}
+            </Box>
+        );
         
         return (
             <>
-                <AppBar position="fixed" color="secondary" elevation={4}>
+                <AppBar position="fixed" color="secondary" elevation={4} open={open}>
                     <Toolbar>
                         <IconButton
                             size="large"
@@ -45,9 +190,14 @@ export default function Header (props) {
                     </Toolbar>
                 </AppBar>
 
-                <Drawer open={open} onClose={toggleDrawer(false)}>
-                    {/* {DrawerList} */}
-                    test
+                <Drawer variant="permanent" open={open} onClose={toggleDrawer(false)}>
+                    <DrawerHeader>
+                        <IconButton onClick={toggleDrawer(false)}>
+                            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+                        </IconButton>
+                    </DrawerHeader>
+                    <Divider />
+                    {DrawerMenu}
                 </Drawer>
             </>
 
